1387b3a5837cbdb6f32fc3c9cc908e46b82d6a12
GuilhermeSilva3176 <guilhermecunha3176@gmail.com>
2024-03-06 15:07:30 -0300

    Add the user Registration in the Site part.

---
+++ Site/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Site.Models;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Site.Controllers;
+
+public class UserController : Controller
+{
+
+    public IActionResult RegisterUser()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterUser(UserRegisterDto dto)
+    {
+        
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                
+                string apiUrl = "https://localhost:7299/api/User/Create";
+                string json = JsonConvert.SerializeObject(dto);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpClient client = new();
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                    return RedirectToAction("Index", "Home");
+                else
+                    ModelState.AddModelError("", "Error registering user. API returned non-success status code.");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("","An error occurred while processing your request.");
+            }
+        }
+        return View();
+    }
+}
---
+++ Site/Models/UserRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Site.Models;
+
+public class UserRegisterDto
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
